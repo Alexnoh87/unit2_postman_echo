bdfe2654ad00cc48678d21a86165d60c948e356d
Alex Nohrin <beterok@inbox.ru>
2023-07-15 17:17:10 +0500

    Successful test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -20,7 +20,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Дратути"))
+                .body("data", equalTo("Hello"))
         ;
     }
 }
